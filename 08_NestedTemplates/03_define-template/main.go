8fdeff2c353459d025df05958bece3435092c24a
Mustafa Katipoğlu <mustafakatipoglu98@hotmail.com>
2018-09-13 22:27:29 +0300

    add nested templates

---
+++ 08_NestedTemplates/03_define-template/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+}
+
+func main() {
+	err := tpl.ExecuteTemplate(os.Stdout, "index.gohtml", nil)
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
